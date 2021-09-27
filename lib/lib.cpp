3a3f8812f391286d0dc5f7d7ac5e9b725e198355
jcake <jakieschneider13@gmail.com>
2021-09-27 23:54:50 +1000

    Segfaults?

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -13,39 +13,43 @@
 static const char *const evval[3] = {"RELEASED", "PRESSED ", "REPEATED"};
 
 Napi::Object open_event(const Napi::CallbackInfo &info) {
-    if (info[0].IsString()) {
-        auto env = info.Env();
+    auto env = info.Env();
 
-        const char *dev = info[0].ToString().Utf8Value().c_str();
+    if (info[0].IsString()) {
+        auto device = info[0].ToString().Utf8Value();
+        const char *dev = device.c_str();
 
         struct input_event ev;
-        ssize_t n;
 
         int fd = open(dev, O_RDONLY);
         if (fd == -1)
-            throw std::string("Cannot open") + dev + ": " + strerror(errno);
+            Napi::TypeError::New(env, std::string("Cannot open ") + dev + ": " + strerror(errno)).ThrowAsJavaScriptException();
+        else {
+            Napi::Object controls = Napi::Object::New(env);
+            controls.Set("close", Napi::Function::New(env, [&](const Napi::CallbackInfo &info) {
+                int status = close(fd);
 
-        Napi::Object controls = Napi::Object::New(env);
-        controls.Set("close", Napi::Function::New(env, [&](const Napi::CallbackInfo &info) {
-            int status = close(fd);
+                if (status != 0)
+                    Napi::TypeError::New(env, std::string("Error closing device") + dev + ": " + strerror(status)).ThrowAsJavaScriptException();
+            }));
+            controls.Set("next", Napi::Function::New(env, [=, &ev](const Napi::CallbackInfo &info) -> Napi::Object {
+                ssize_t n = read(fd, &ev, sizeof ev);
 
-            if (status != 0)
-                throw std::string("Error closing device") + dev + ": " + strerror(status);
-        }));
-        controls.Set("next", Napi::Function::New(env, [&](const Napi::CallbackInfo &info) -> Napi::Object {
-            n = read(fd, &ev, sizeof ev);
+                Napi::Object event = Napi::Object::New(env);
+                event.Set("time", Napi::Number::New(env, (double)(ev.time.tv_usec / 1000)));
+                event.Set("type", Napi::Number::New(env, (double)ev.type));
+                event.Set("code", Napi::Number::New(env, (double)ev.code));
+                event.Set("value", Napi::Number::New(env, (double)ev.value));
 
-            Napi::Object event = Napi::Object::New(env);
-            event.Set("time", Napi::Number::New(env, (double)(ev.time.tv_usec / 1000)));
-            event.Set("type", Napi::Number::New(env, (double)ev.type));
-            event.Set("code", Napi::Number::New(env, (double)ev.code));
-            event.Set("value", Napi::Number::New(env, (double)ev.value));
+                return event;
+            }));
 
-            return event;
-        }));
+            return controls;
+        }
+    } else
+        Napi::TypeError::New(env, "Expected string").ThrowAsJavaScriptException();
 
-        return controls;
-    } else throw "Expected string";
+    return Napi::Object::New(env);
 }
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
